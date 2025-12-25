3941f2c3244af62c8f7422e66f626141a84edea4
kblaszke <krzysztof.blaszke@gmail.com>
2025-12-25 18:43:27 +0100

    Initial commit: Spring Boot + devenv

---
+++ devenv.nix
@@ -0,0 +1,66 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ 
+	temurin-bin-17
+        git
+        curl
+        zsh
+        oh-my-zsh
+        coreutils
+        maven
+        gradle
+        docker
+        docker-compose
+        jq
+        tree
+];
+
+env = {
+    JAVA_HOME = "${pkgs.temurin-bin-17}";
+    SPRING_PROFILES_ACTIVE = "local";
+  };
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+	hello         # Run scripts directly
+    echo "☕ Java: $(java -version 2>&1 | head -n 1)"
+    git --version # Use packages
+    mvn --version
+'';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
