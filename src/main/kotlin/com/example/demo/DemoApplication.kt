1646187e47e7d5af729aa1589552c04fe5aef310
kblaszke <krzysztof.blaszke@gmail.com>
2025-12-27 14:56:42 +0100

    [feature/helloController] add simple controller

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @SpringBootApplication
 class DemoApplication
---
+++ src/main/kotlin/com/example/demo/controller/HelloWorldController.kt
@@ -0,0 +1,20 @@
+package com.example.demo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World!"
+    }
+
+    @PostMapping("/hello")
+    fun helloPost(@RequestBody name: String): String {
+        return "Hello $name!"
+    }
+}
